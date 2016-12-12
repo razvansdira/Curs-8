4536c95aeb3f5e65d8095b058dd87b5133bfcba3
razvan.sdira <sdira_razvan@yahoo.com>
2016-12-12 21:05:59 +0200

    Curs 9

--- Curs8 MVC/Models/ProjectEvaluation.cs
+++ Curs8 MVC/Models/ProjectEvaluation.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Curs8_MVC.Models
 {
+    [Bind(Exclude = "Country")]
+    //[Bind(Include = "all the other properties")]
+
     public class ProjectEvaluation
-    {
+    {      
         public string name { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
         public string city { get; set; }
+
         public string country { get; set; }
+
+        [Range(1, 10)]
+        [Required]
         public double rating { get; set; }
 
         public int id { get; set; }
